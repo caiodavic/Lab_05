0270729e63a75ff23c9a6b539724e87b44b2b1bb
caiodavic <caio.silva@ccc.ufcg.edu.br>
2019-09-27 19:26:28 -0300

    Classe Cliente pronta

--- LAB_5_PARTE1_CAIO_DAVI/src/lab5/Controller.java
+++ LAB_5_PARTE1_CAIO_DAVI/src/lab5/Controller.java
@@ -6,66 +6,95 @@ import util.Validacao;
 
 public class Controller {
 
-		private HashMap<String,Cliente> clientes;
-		
-		private Validacao validador;
-		
-		public Controller() {
-			this.clientes = new HashMap<>();
-			this.validador = new Validacao();
-			
-			
-		}
-		
-		public String cadastraClientes(String cpf, String nome, String email, String loc) {
-			String cpfreturn = ""; 
-			
-			validador.validaNulleVazio(cpf);
-			validador.validaNulleVazio(nome);
-			validador.validaNulleVazio(email);
-			validador.validaNulleVazio(loc);
-			
-			if( !clientes.containsKey(cpf)) {
-				Cliente clienteaux = new Cliente(cpf,nome,email,loc);
-				 clientes.put(cpf, clienteaux);
-				 cpfreturn = cpf;
-			 } else {
-				 
-				 cpfreturn = "CPF JÁ CADASTRADO!";
-			 }
-			
-			return cpfreturn;
-			
-		}
-		
-		public String exibeCliente(String cpf) {
-			String msg = "";
-			validador.validaNulleVazio(cpf);
-			
-			if(clientes.containsKey(cpf)) {
-				msg = clientes.get(cpf).toString();
-			} else {
-				msg = "CPF NÃO CADASTRADO!";
-			}
-			
-			return msg;
-		}
-		
-		public String exibeTodosClientes() {
-			String msg = "";
-			
-			if(!clientes.isEmpty()) {
-					for(Cliente clienteaux: this.clientes.values()) {
-						msg = clienteaux.toString() + "|";
-						
-					}
-				}
-			
-			return msg;
-			}	
-		
-		
-		
-		
-}
+	private HashMap<String, Cliente> clientes;
 
+	private Validacao validador;
+
+	public Controller() {
+		this.clientes = new HashMap<>();
+		this.validador = new Validacao();
+
+	}
+
+	public String cadastraClientes(String cpf, String nome, String email, String loc) {
+		String cpfreturn = "";
+
+		validador.validaNulleVazio(cpf);
+		validador.validaNulleVazio(nome);
+		validador.validaNulleVazio(email);
+		validador.validaNulleVazio(loc);
+
+		if (!clientes.containsKey(cpf)) {
+			Cliente clienteaux = new Cliente(cpf, nome, email, loc);
+			clientes.put(cpf, clienteaux);
+			cpfreturn = cpf;
+		} else {
+
+			cpfreturn = "CPF JÁ CADASTRADO!";
+		}
+
+		return cpfreturn;
+
+	}
+
+	public String exibeCliente(String cpf) {
+		String msg = "";
+		validador.validaNulleVazio(cpf);
+
+		if (clientes.containsKey(cpf)) {
+			msg = clientes.get(cpf).toString();
+		} else {
+			msg = "CPF NÃO CADASTRADO!";
+		}
+
+		return msg;
+	}
+
+	public String exibeTodosClientes() {
+		String msg = "";
+
+		if (!clientes.isEmpty()) {
+			for (Cliente clienteaux : this.clientes.values()) {
+				msg = clienteaux.toString() + "|";
+
+			}
+		}
+
+		return msg;
+	}
+
+	public void editaCadastro(String cpf, String oqAltera, String novoDado) {
+		oqAltera = oqAltera.toUpperCase();
+
+		if (clientes.containsKey(cpf)) {
+			switch (oqAltera) {
+			case "NOME":
+				clientes.get(cpf).setNome(novoDado);
+				break;
+
+			case "LOCALIZAÇÃO":
+				clientes.get(cpf).setLoc(novoDado);
+				break;
+
+			case "EMAIL":
+				clientes.get(cpf).setEmail(novoDado);
+				break;
+
+			case "CPF":
+				System.out.println("Impossível alterar CPF");
+				break;
+
+			default:
+				System.out.println("Dado inexistente");
+				break;
+			}
+		}
+	}
+
+	public void removeCliente(String cpf) {
+			if(clientes.containsKey(cpf)) {
+				clientes.remove(cpf);
+			}
+		}
+
+}
